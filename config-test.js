b9aa466e09ab33da6a81e2b2f0ea3b08478b6859
renxj <renxiaojuan@suiyueyule.com>
2018-09-12 10:27:42 +0800

    update

---
+++ config-test.js
@@ -0,0 +1,10 @@
+var config = {
+    dialect: 'mysql',
+    database: 'test',
+    username: 'root',
+    password: '',
+    host: '127.0.0.1',
+    port: 3306
+};
+console.log('test');
+module.exports = config;
